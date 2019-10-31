92512f467649be62d8b595596c280d13882116b0
wuxiaoqi <1321972760@qq.com>
2019-10-31 14:30:29 +0800

    权限

---
+++ app/src/main/java/com/welcome/jetpack_learn/app/JetPackApplication.kt
@@ -0,0 +1,12 @@
+package com.welcome.jetpack_learn.app
+
+import android.app.Application
+
+class JetPackApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}

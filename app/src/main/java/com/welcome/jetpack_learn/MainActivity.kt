9d0a658e71d9d2cf36c2a95cc930e1a97524d67b
wuxiaoqi <1321972760@qq.com>
2019-11-24 17:14:19 +0800

    statusbarutil

--- app/src/main/java/com/welcome/jetpack_learn/MainActivity.kt
+++ app/src/main/java/com/welcome/jetpack_learn/MainActivity.kt
@@ -2,11 +2,13 @@ package com.welcome.jetpack_learn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.welcome.jetpack_learn.utils.StatusBarUtil
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        StatusBarUtil.setLightMode(this)
         setContentView(R.layout.activity_main)
     }
 }

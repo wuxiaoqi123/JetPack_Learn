e44b43d43137754f104af9a7136dadddfb22b571
wuxiaoqi <1321972760@qq.com>
2019-12-10 01:54:49 +0800

    navigation

--- app/src/main/java/com/welcome/jetpack_learn/ui/navigation/fg/SampleNotificationFragment.kt
+++ app/src/main/java/com/welcome/jetpack_learn/ui/navigation/fg/SampleNotificationFragment.kt
@@ -1,11 +1,19 @@
 package com.welcome.jetpack_learn.ui.navigation.fg
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.welcome.jetpack_learn.R
+import com.welcome.jetpack_learn.ext.snackBarShow
+import kotlinx.android.synthetic.main.fragment_sample_notification.*
 
 class SampleNotificationFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +22,41 @@ class SampleNotificationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_sample_notification, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tv_deep_link_args_hint.text = arguments?.getString("deep_args")
+        btn_nav_dashboard_jump_home.setOnClickListener {
+            findNavController().navigate(R.id.action_notificationSampleFragment_to_homeSampleFragment)
+        }
+        btn_nav_deep_link.setOnClickListener {
+            val args = Bundle()
+            args.putString("deep_args", et_deep_link.text.toString())
+            val deep = findNavController().createDeepLink()
+                .setDestination(R.id.notificationSampleFragment)
+                .setArguments(args)
+                .createPendingIntent()
+            val notificationManager =
+                context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationManager.createNotificationChannel(
+                    NotificationChannel(
+                        "deeplink",
+                        "Deep Links", NotificationManager.IMPORTANCE_HIGH
+                    )
+                )
+            }
+            val builder = NotificationCompat.Builder(
+                context!!, "deeplink"
+            )
+                .setContentTitle(resources.getString(R.string.app_name))
+                .setContentText("Navigation 深层链接测试")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(deep)
+                .setAutoCancel(true)
+            notificationManager.notify(0, builder.build())
+            requireActivity().snackBarShow(view, getString(R.string.deeplink_hint))
+        }
     }
 }

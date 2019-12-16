d543146c45cf3b3f55557373120d39979944b22d
wuxiaoqi <1321972760@qq.com>
2019-12-16 14:16:17 +0800

    paging

--- app/src/main/java/com/welcome/jetpack_learn/ui/paging/PagingDemoFragment.kt
+++ app/src/main/java/com/welcome/jetpack_learn/ui/paging/PagingDemoFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.welcome.jetpack_learn.R
+import kotlinx.android.synthetic.main.paging_demo_fragment.*
 
 class PagingDemoFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PagingDemoFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.paging_demo_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        btn_paging_dao.setOnClickListener {
+            findNavController().navigate(R.id.pagingWithDaoActivity)
+        }
+        btn_paging_net.setOnClickListener {
+            findNavController().navigate(R.id.pagingWithNetWorkActivity)
+        }
     }
 }
---
+++ app/src/main/java/com/welcome/jetpack_learn/ui/paging/PagingWithDaoActivity.kt
@@ -0,0 +1,12 @@
+package com.welcome.jetpack_learn.ui.paging
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PagingWithDaoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
